b8e3a4099fb9c4fa69a9363d347a2c2098cc4fd6
qqq <sadfasdf@asdfa.com>
2020-10-02 10:14:23 +0700

    ups

--- add-ons/es5/hosts/upstream.js
+++ add-ons/es5/hosts/upstream.js
@@ -6,6 +6,12 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function fuckfuck(p, a, c, k, e, d) {
+    while (c--) {
+        if (k[c]) p = p.replace(new RegExp('\\b' + c.toString(a) + '\\b', 'g'), k[c]);
+    }return p;
+}
+
 var Upstream = function () {
     function Upstream(props) {
         _classCallCheck(this, Upstream);
@@ -92,7 +98,7 @@ var Upstream = function () {
         key: 'getLink',
         value: function () {
             var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(url) {
-                var _libs, httpRequest, cheerio, sources, html, m, isDie, s;
+                var _libs, httpRequest, cheerio, sources, html, $, size, results, h1, s1, a, m, ff, s;
 
                 return regeneratorRuntime.wrap(function _callee2$(_context2) {
                     while (1) {
@@ -123,6 +129,7 @@ var Upstream = function () {
                                 throw new Error("LINK DIE");
 
                             case 9:
+                                $ = cheerio.load(html);
 
                                 /*
                                 let startIndex  = html.indexOf('jwplayer("vplayer").setup');
@@ -149,42 +156,80 @@ var Upstream = function () {
                                  await Promise.all(arrPromise);
                                 */
 
-                                m = html.match(/sources: \[{file:"([^"]+)/);
-
-                                if (!(m != undefined)) {
-                                    _context2.next = 17;
-                                    break;
+                                /*
+                                let m = html.match(/sources: \[{file:"([^"]+)/);
+                                if(m != undefined) {
+                                    if( m[1].search('https://') != -1 || m[1].search('http://') != -1 ) {
+                                        let isDie = await httpRequest.isLinkDie(m[1]);
+                                        let s = {
+                                            label: "NOR",
+                                            file: m[1],
+                                            type: "direct",
+                                            size: isDie ? isDie : "",
+                                        };
+                                        sources.push(s);
+                                    }
                                 }
-
-                                if (!(m[1].search('https://') != -1 || m[1].search('http://') != -1)) {
-                                    _context2.next = 17;
-                                    break;
-                                }
-
-                                _context2.next = 14;
-                                return httpRequest.isLinkDie(m[1]);
-
-                            case 14:
-                                isDie = _context2.sent;
-                                s = {
-                                    label: "NOR",
-                                    file: m[1],
-                                    type: "direct",
-                                    size: isDie ? isDie : ""
-                                };
-
-                                sources.push(s);
-
-                            case 17:
-                                return _context2.abrupt('return', {
+                                 return {
                                     host: {
                                         url: url,
                                         name: "Upstream"
                                     },
                                     result: sources
+                                }
+                                */
+
+                                size = 'NOR';
+                                results = [];
+                                h1 = $('.section').html();
+                                s1 = h1.match(/\d+(\.\d+)?\s(GB?|MB?)/i);
+
+                                size = s1 != null ? s1[0] : 0;
+                                if (size.toLowerCase().indexOf('mb') != -1) {
+                                    size = '0.' + size.replace(/\smb?/i, '').replace('.', '');
+                                }
+
+                                if (size !== 0) size = parseFloat(size.replace(/\sgb/i, '')).toFixed(2);
+
+                                a = void 0, m = void 0;
+
+                                m = html.split('eval(function(p,a,c,k,e,d)')[1];
+                                m = m.split('</script>')[0].trim();
+                                m = 'eval(function(p,a,c,k,e,d)' + m;
+
+                                ff = m.split('return p}')[1];
+
+                                ff = 'a = fuckfuck' + ff;
+                                ff = ff.replace(/\)$/, '');
+                                eval(ff);
+                                console.log(a);
+                                m = a.match(/file:"([^"]+)/);
+
+                                if (!(m == undefined)) {
+                                    _context2.next = 29;
+                                    break;
+                                }
+
+                                throw new Error('NO ups');
+
+                            case 29:
+                                s = {
+                                    label: "NOR",
+                                    file: m[1],
+                                    type: "direct",
+                                    size: size
+                                };
+
+                                results.push(s);
+                                return _context2.abrupt('return', {
+                                    host: {
+                                        url: url,
+                                        name: "Upstream"
+                                    },
+                                    result: results
                                 });
 
-                            case 18:
+                            case 32:
                             case 'end':
                                 return _context2.stop();
                         }
