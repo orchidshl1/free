64761b87ec349481ebcb019112294af1b6ffebe4
Frank Castle <frankcastle.clone@gmail.com>
2022-08-22 14:33:13 +0700

    Fix provider fembed

--- add-ons/es5/hosts/fembed.js
+++ add-ons/es5/hosts/fembed.js
@@ -7,163 +7,163 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Fembed = function () {
-    function Fembed(props) {
-        _classCallCheck(this, Fembed);
+  function Fembed(props) {
+    _classCallCheck(this, Fembed);
 
-        this.libs = props.libs;
-        this.settings = props.settings;
-        this.state = {};
-    }
+    this.libs = props.libs;
+    this.settings = props.settings;
+    this.state = {};
+  }
 
-    _createClass(Fembed, [{
-        key: 'checkLive',
-        value: function () {
-            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url) {
-                var httpRequest, html;
-                return regeneratorRuntime.wrap(function _callee$(_context) {
-                    while (1) {
-                        switch (_context.prev = _context.next) {
-                            case 0:
-                                httpRequest = this.libs.httpRequest;
-                                _context.next = 3;
-                                return httpRequest.post(url);
+  _createClass(Fembed, [{
+    key: "checkLive",
+    value: function () {
+      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url) {
+        var httpRequest, html;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                httpRequest = this.libs.httpRequest;
+                _context.next = 3;
+                return httpRequest.post(url);
 
-                            case 3:
-                                html = _context.sent;
-                                return _context.abrupt('return', html);
+              case 3:
+                html = _context.sent;
+                return _context.abrupt("return", html);
 
-                            case 5:
-                            case 'end':
-                                return _context.stop();
-                        }
-                    }
-                }, _callee, this);
-            }));
-
-            function checkLive(_x) {
-                return _ref.apply(this, arguments);
+              case 5:
+              case "end":
+                return _context.stop();
             }
+          }
+        }, _callee, this);
+      }));
 
-            return checkLive;
-        }()
-    }, {
-        key: 'getLink',
-        value: function () {
-            var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(url) {
-                var _libs, httpRequest, cheerio, s, id, u, html, data, results, arrPromise;
+      function checkLive(_x) {
+        return _ref.apply(this, arguments);
+      }
 
-                return regeneratorRuntime.wrap(function _callee3$(_context3) {
-                    while (1) {
-                        switch (_context3.prev = _context3.next) {
-                            case 0:
-                                _libs = this.libs, httpRequest = _libs.httpRequest, cheerio = _libs.cheerio;
-                                //console.log('xxxxxx start fembed');
+      return checkLive;
+    }()
+  }, {
+    key: "getLink",
+    value: function () {
+      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(url) {
+        var _libs, httpRequest, cheerio, s, id, u, html, data, results, arrPromise;
 
-                                _context3.prev = 1;
+        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+          while (1) {
+            switch (_context3.prev = _context3.next) {
+              case 0:
+                _libs = this.libs, httpRequest = _libs.httpRequest, cheerio = _libs.cheerio;
+                //console.log('xxxxxx start fembed');
 
-                                url = url.replace(/#.*$/, '');
+                _context3.prev = 1;
 
-                                s = url.split('/');
-                                id = s[s.length - 1];
-                                u = 'https://suzihaza.com/api/source/' + id;
-                                _context3.next = 8;
-                                return httpRequest.post(u, {
-                                    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-                                }, {
-                                    r: '',
-                                    d: 'suzihaza.com'
-                                });
+                url = url.replace(/#.*$/, "");
 
-                            case 8:
-                                html = _context3.sent;
-                                data = html.data.data;
-                                results = [];
-                                //console.log('xxxxxx' + 'get data');
+                s = url.split("/");
+                id = s[s.length - 1];
+                u = "https://vanfem.com/api/source/" + id;
+                _context3.next = 8;
+                return httpRequest.post(u, {
+                  "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+                }, {
+                  r: "",
+                  d: "vanfem.com"
+                });
 
-                                arrPromise = data.map(function () {
-                                    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(val) {
-                                        var label, isDie;
-                                        return regeneratorRuntime.wrap(function _callee2$(_context2) {
-                                            while (1) {
-                                                switch (_context2.prev = _context2.next) {
-                                                    case 0:
-                                                        label = val.label;
-                                                        _context2.next = 3;
-                                                        return httpRequest.isLinkDie(val.file);
+              case 8:
+                html = _context3.sent;
+                data = html.data.data;
+                results = [];
+                //console.log('xxxxxx' + 'get data');
 
-                                                    case 3:
-                                                        isDie = _context2.sent;
+                arrPromise = data.map(function () {
+                  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(val) {
+                    var label, isDie;
+                    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+                      while (1) {
+                        switch (_context2.prev = _context2.next) {
+                          case 0:
+                            label = val.label;
+                            _context2.next = 3;
+                            return httpRequest.isLinkDie(val.file);
+
+                          case 3:
+                            isDie = _context2.sent;
 
 
-                                                        if (isDie != false) {
-                                                            console.log('xxxxxx' + 'push link data');
-                                                            results.push({
-                                                                file: val.file,
-                                                                label: label,
-                                                                type: "direct",
-                                                                size: isDie
-                                                            });
-                                                        }
+                            if (isDie != false) {
+                              console.log("xxxxxx" + "push link data");
+                              results.push({
+                                file: val.file,
+                                label: label,
+                                type: "direct",
+                                size: isDie
+                              });
+                            }
 
-                                                    case 5:
-                                                    case 'end':
-                                                        return _context2.stop();
-                                                }
-                                            }
-                                        }, _callee2, this);
-                                    }));
-
-                                    return function (_x3) {
-                                        return _ref3.apply(this, arguments);
-                                    };
-                                }());
-                                //console.log('xxxxxx' + 'day promise xu ly');
-
-                                _context3.next = 14;
-                                return Promise.all(arrPromise);
-
-                            case 14:
-                                return _context3.abrupt('return', {
-                                    host: {
-                                        url: url,
-                                        name: "Fembed"
-                                    },
-                                    result: results
-                                });
-
-                            case 17:
-                                _context3.prev = 17;
-                                _context3.t0 = _context3['catch'](1);
-                                return _context3.abrupt('return', {
-                                    host: {
-                                        url: url,
-                                        name: "Fembed"
-                                    },
-                                    result: []
-                                });
-
-                            case 20:
-                            case 'end':
-                                return _context3.stop();
+                          case 5:
+                          case "end":
+                            return _context2.stop();
                         }
-                    }
-                }, _callee3, this, [[1, 17]]);
-            }));
+                      }
+                    }, _callee2, this);
+                  }));
 
-            function getLink(_x2) {
-                return _ref2.apply(this, arguments);
+                  return function (_x3) {
+                    return _ref3.apply(this, arguments);
+                  };
+                }());
+                //console.log('xxxxxx' + 'day promise xu ly');
+
+                _context3.next = 14;
+                return Promise.all(arrPromise);
+
+              case 14:
+                return _context3.abrupt("return", {
+                  host: {
+                    url: url,
+                    name: "Fembed"
+                  },
+                  result: results
+                });
+
+              case 17:
+                _context3.prev = 17;
+                _context3.t0 = _context3["catch"](1);
+                return _context3.abrupt("return", {
+                  host: {
+                    url: url,
+                    name: "Fembed"
+                  },
+                  result: []
+                });
+
+              case 20:
+              case "end":
+                return _context3.stop();
             }
+          }
+        }, _callee3, this, [[1, 17]]);
+      }));
 
-            return getLink;
-        }()
-    }]);
+      function getLink(_x2) {
+        return _ref2.apply(this, arguments);
+      }
 
-    return Fembed;
+      return getLink;
+    }()
+  }]);
+
+  return Fembed;
 }();
 
 thisSource.function = function (libs, settings) {
-    return new Fembed({
-        libs: libs,
-        settings: settings
-    });
+  return new Fembed({
+    libs: libs,
+    settings: settings
+  });
 };
